9b6cdd0b6bdae3735f47b132f951c29b0c573473
Christian <christian0511@gmx.de>
2025-05-29 16:21:07 +0200

    internal logger crate

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,12 @@
+use logger::debug;
+
+#[derive(Debug)]
+pub enum RuntimeOptions {}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+pub fn run(options: &Vec<RuntimeOptions>) {
+    debug!("Running Tauri application with options: {:?}", options);
+
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use clap::{arg, command, crate_name, crate_version, ArgAction};
+
 fn main() {
-    aula_assistant_lib::run()
+    let matches = command!()
+        // help
+        .next_line_help(false)
+        // version
+        .disable_version_flag(true)
+        .arg(arg!(-v --version "Print version").action(ArgAction::SetTrue))
+        // license
+        .arg(arg!(--license "Print license information").action(ArgAction::SetTrue))
+        // matches
+        .get_matches();
+
+    if matches.get_flag("version") {
+        println!(
+            "{} {} ({} {})",
+            crate_name!(),
+            crate_version!(),
+            env!("LAST_COMMIT_ID"),
+            env!("LAST_COMMIT_DATE"),
+        );
+        std::process::exit(0);
+    }
+
+    if matches.get_flag("license") {
+        println!(include_str!("../../LICENSE.txt"));
+        std::process::exit(0);
+    }
+
+    let runtime_options = vec![];
+
+    aula_assistant_lib::run(&runtime_options);
 }
